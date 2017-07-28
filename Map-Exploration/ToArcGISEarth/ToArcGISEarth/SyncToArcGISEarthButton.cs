65eb28702f677990566594afed38313d14c53d2e
Yongwei Kang <YKang@esri.com>
2017-07-28 11:04:55 +0800

    Add ToArcGISEarth AddIn

---
+++ Map-Exploration/ToArcGISEarth/ToArcGISEarth/SyncToArcGISEarthButton.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArcGIS.Desktop.Framework;
+using ArcGIS.Desktop.Framework.Contracts;
+using ArcGIS.Desktop.Mapping.Events;
+using ArcGIS.Desktop.Mapping;
+
+namespace ToArcGISEarth
+{
+    internal class SyncToArcGISEarthButton : Button
+    {
+        private SyncToArcGISEarthButton()
+        {
+        }
+
+        protected override void OnClick()
+        {
+            if (this.IsChecked)
+            {
+                MapViewCameraChangedEvent.Unsubscribe(MapViewCameraCanged);
+                this.IsChecked = false;
+                this.Caption = "Activate";
+
+            }
+            else
+            {
+                MapViewCameraChangedEvent.Subscribe(MapViewCameraCanged, false);
+                this.IsChecked = true;
+                this.Caption = "Deactivate";
+            }
+        }
+
+        private void MapViewCameraCanged(MapViewCameraChangedEventArgs args)
+        {
+            MapView mapView = args.MapView;
+            if (null != mapView && null != mapView.Camera)
+            {
+            }
+        }
+    }
+}
